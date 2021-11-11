183ccd90af039dc9fb6c8c8bd842638ab0db4344
Aamer Abdul Rahman <ar.aamer@gmail.com>
2021-11-11 12:42:09 +0400

    env

--- jobs_test/proto_conv64/eurosat_1shot.sh
+++ jobs_test/proto_conv64/eurosat_1shot.sh
@@ -17,7 +17,7 @@
 nvidia-smi
 
 module load python/3.7
-source ~/my-env4/bin/activate
+source ~/my_env5/bin/activate
 
 echo "------------------------------------< Data preparation>----------------------------------"
 echo "Copying the source code"
